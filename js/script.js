82a0b6ab33731831762d5cb8f7de9e6a764e1922
totarochristian <totaro.christian.18@gmail.com>
2023-03-30 16:03:54 +0200

    Define the method AddQuests that will add a new quest in the quests array using the value passed in the input element.

--- js/script.js
+++ js/script.js
@@ -9,6 +9,12 @@ createApp({
         }
     },
     methods:{
-
+        AddQuests(){
+            if(newQuest.value){
+                const obj = { id: 'quest_' + (this.lastQuestId += 1), toDo: newQuest.value, done: 0 };
+                this.quests.push(obj);
+                this.newQuest = '';
+            }
+        }
     }
 }).mount("#app");
